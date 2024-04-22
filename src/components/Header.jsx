dc34b3f5e781c0f28408308c2389a30ddc6be256
badal <badal@badals-MacBook-Air.local>
2024-04-22 23:14:55 +0530

    robocor page added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,6 +20,11 @@ function Header() {
               Home
             </li>
           </Link>
+          <Link to="https://robocor24.cyclic.app/" spy={true} smooth={true}>
+            <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
+              Robocor'24
+            </li>
+          </Link>
           <Link to="About" spy={true} smooth={true}>
             <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded">
               About
@@ -96,6 +101,16 @@ function Header() {
               onClick={() => handleTabClick("Home")}
             >
               Home
+            </li>
+          </Link>
+          <Link to="https://robocor24.cyclic.app/">
+            <li
+              className={`nav-item ${
+                activeTab === "Robocor'24" ? "active" : ""
+              }`}
+              onClick={() => handleTabClick("Robocor'24")}
+            >
+              Robocor'24
             </li>
           </Link>
           <Link to="/projects">
